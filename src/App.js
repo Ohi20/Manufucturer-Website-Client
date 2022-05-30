e5fba02bc062fa80ea48d6bf4b88026189f7d9ff
Ohi20 <ashrafohi20@gmail.com>
2022-05-30 15:33:45 +0600

    pages-created

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Shared/Navbar';
 import Home from './Pages/Home/Home';
-import Purchase from './Appointment/Purchase';
+import Purchase from './Pages/Appointment/Purchase';
+import Dashboard from './Pages/Dashboard';
+import NotFound from './Pages/NotFound';
+import Blogs from './Pages/Blogs/Blogs';
+import Portfolio from './Pages/Portfolio/Portfolio';
 
 function App() {
   return (
-    <div>
+    <div className='max-w-7xl mx-auto px-12'>
       <Navbar></Navbar>
        <Routes>
         <Route path="/" element={<Home></Home>} />
         <Route path="/purchase" element={<Purchase></Purchase>} />
+        <Route path="/dashboard" element={<Dashboard></Dashboard>} />
+        <Route path="/blogs" element={<Blogs></Blogs>} />
+        <Route path="/portfolio" element={<Portfolio></Portfolio>} />
+        <Route path="*" element={<NotFound></NotFound>} />
         
       </Routes>
     </div>
---
+++ src/Pages/NotFound.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Footer from '../Shared/Footer';
+
+const NotFound = () => {
+    return (
+        <div>
+            <div className='mt-10'>
+            <h2 className='text-center font-bold text-red-700 mb-10 text-xl'>Not Found</h2>
+            <div>
+            <div class="mockup-code">
+  <pre data-prefix=">" class="text-red-500 font-bold text-xl"><code>The Page You Are Looking For Is Not Found</code></pre>
+  <pre data-prefix="*" class="text-gray-100 font-bold text-xl"><code>404</code></pre> 
+  <pre data-prefix="?" class="text-warning font-bold text-xl"><code>Please try a different Route</code></pre> 
+  <pre data-prefix="!" class="text-green-500 font-bold text-xl"><code>Provide the link carefully</code></pre>
+</div>
+            </div>
+            
+        </div>
+
+        <div className='text-center mt-8 mb-5'>
+            <h2 className='text-green-500 font-bold mb-5'>Click Here For Fun</h2>
+        <label class="swap swap-flip text-9xl">
+  
+  
+  <input type="checkbox" />
+  
+  <div class="swap-on">😈</div>
+  <div class="swap-off">😇</div>
+</label>
+        </div>
+        <Footer></Footer>
+        </div>
+    );
+};
+
+export default NotFound;
